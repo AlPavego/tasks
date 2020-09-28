983a59a89d0cd6360ae3b7c2747ff97983376a06
AlPavego <mr.pax@inbox.ru>
2020-09-28 15:34:08 +0500

    Added 1.7 tasks

--- src/m01/s17/T4.java
+++ src/m01/s17/T4.java
@@ -2,7 +2,8 @@ package m01.s17;
 
 import java.util.Scanner;
 
-public class T4 {
+public class T4
+{
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
--- src/m01/s17/T9.java
+++ src/m01/s17/T9.java
@@ -2,7 +2,8 @@ package m01.s17;
 
 import java.util.Scanner;
 
-public class T9 {
+public class T9
+{
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
