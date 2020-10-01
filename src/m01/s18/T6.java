1c2d77151af8f1285ad9c7acd0802407c6948bba
Pavel <mr.pax@inbox.ru>
2020-10-01 20:13:38 +0500

    Added s1.8 6-7 tasks

--- src/m01/s18/T6.java
+++ src/m01/s18/T6.java
@@ -1,7 +1,40 @@
 package m01.s18;
 
+import java.util.Scanner;
+
 public class T6 {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] arr, quant;
+        int max;
+        arr = new int [in.nextInt()];
+        quant = new int [arr.length];
 
+
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = in.nextInt();
+        }
+
+        for (int i = 0; i < arr.length; i++){
+            int count = 0;
+            for (int k : arr) {
+                if (arr[i] == k)
+                    count++;
+            }
+            quant[i] = count;
+        }
+
+        max = quant[0];
+        for (int num: quant){
+            if (max < num)
+                max = num;
+        }
+
+        for(int i = 0; i < arr.length; i++){
+            if (quant[i] == max){
+                System.out.println(arr[i]);
+                break;
+            }
+        }
     }
 }
--- src/m01/s18/T7.java
+++ src/m01/s18/T7.java
@@ -1,7 +1,39 @@
 package m01.s18;
 
+import java.util.Scanner;
+
 public class T7 {
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
+        int[] arr;
+        int count = 0;
+        int first_c = -1;
+        arr = new int [in.nextInt()];
+
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = in.nextInt();
+        }
+
+        for (int k: arr){
+            if (k < 0)
+                first_c++;
+            else
+                break;
+        }
+
+        for (int i = first_c + 1; i < arr.length; i++){
+            if (arr[i] < 0){
+                int temp = arr[i];
+                for (int j = i; j > first_c + 1; j--){
+                    arr[j] = arr[j - 1];
+                }
+                arr[first_c + 1] = temp;
+                first_c++;
+            }
+        }
+
+        for(int k: arr){
+            System.out.print(k + " ");
+        }
     }
 }
